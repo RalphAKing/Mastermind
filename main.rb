b4b29166aa3fce47857efdae8bf859e370bbe2e1
RalphAKing <2019rking@beauchamp.org.uk>
2024-09-13 09:14:16 +0100

    Add files via upload

--- main.rb
+++ main.rb
@@ -1,53 +1,65 @@
-def comparearrays(code, input)
-  pmach=0
-  mach=0
-  index=0
-  for i in code
-    if input[index] == i
-      pmach+=1
+class String
+    def green;          "\e[32m#{self}\e[0m" end
+    def blue;           "\e[34m#{self}\e[0m" end
     end
-    index+=1
-  end
-  cindex=0
-  maches=[]
-  for i in code
-    for j in input
-      if i==j
-        found=0
-        for q in maches
-          if q==cindex
-            found=1
-          end
+    
+    def comparearrays(code, input)
+      pmach=0
+      mach=0
+      index=0
+      for i in code
+        if input[index] == i
+          pmach+=1
         end
-        if found == 0
-          maches.append(cindex)
-          mach+=1
-        end
+        index+=1
       end
-    end 
-    cindex+=1
-  end
-  puts "Perfect matches #{pmach}\nMatches #{mach-pmach}"
-end
-
-CODES = %w[1 2 3 4 5 6]
-code = Array.new(4) { CODES.sample }
-
-run = 0
-while run != 10
-  puts 'enter a number'
-  userinput = gets.chomp.chars
-
-  if userinput.length == 4
-    if code == userinput
-      puts 'you won'
-      run = 10
-    else
-      comparearrays(code, userinput)
-      run += 1
+      cindex=0
+      maches=[]
+      for i in code
+        for j in input
+          if i==j
+            found=0
+            for q in maches
+              if q==cindex
+                found=1
+              end
+            end
+            if found == 0
+              maches.append(cindex)
+              mach+=1
+            end
+          end
+        end 
+        cindex+=1
+      end
+      puts 'hints: ' + '•'.green*pmach + '•'.blue*(mach-pmach)
     end
-  else
-    puts 'Invalid Input'
-  end
-  
-end
+    
+    CODES = %w[1 2 3 4 5 6]
+    code = Array.new(4) { CODES.sample }
+    puts 'You need to guess the right 4 digit code.'
+    puts 'the numbers are from 1-6'
+    puts 'You will get 10 attempts.'
+    puts 'On each go you get given hints'
+    puts 'Perfect match ' + '•'.green
+    puts 'Match ' + '•'.blue
+    
+    run = 0
+    while run != 10
+      puts 'enter a number:'
+      userinput = gets.chomp.chars
+    
+      if userinput.length == 4
+        if code == userinput
+          puts 'you won'
+          run = 10
+        else
+          comparearrays(code, userinput)
+          run += 1
+        end
+      else
+        puts 'Invalid Input'
+      end
+      
+    end
+    
